e0f726dc0ba34a3597384aaa7a35f702d784e883
Adam Banaś <242353@edu.p.lodz.pl>
2024-03-03 18:14:42 +0100

    Added Rectangular and Rectangular Symmetric Signals

--- DSP/zad1/SignalGenerator.py
+++ DSP/zad1/SignalGenerator.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.signal import square
 
 class SinusSignal:
 
     def __init__(self, amplitude, frequency, phase, t1, d):
         self.amplitude = amplitude
-        self.frequnecy = frequency
+        self.frequency = frequency
         self.phase = phase
         self.t1 = t1
         self.duration = d
@@ -38,7 +38,8 @@ class SinusSignal:
 
     @staticmethod
     def generate_sinus_signal_half(amplitude, frequency, phase, t):
-        return amplitude/2 * (np.sin(2 * np.pi * frequency * t + phase) + abs(np.sin(2 * np.pi * frequency * t + phase)))
+        return amplitude / 2 * (np.sin(2 * np.pi * frequency * t + phase)
+                                + abs(np.sin(2 * np.pi * frequency * t + phase)))
 
     @staticmethod
     def plot_sinus_signal_half(amplitude, frequency, phase, t1, d):
@@ -66,6 +67,7 @@ class SinusSignal:
         plt.grid(True)
         plt.show()
 
+
 class UniformNoise:
 
     def __init__(self, amplitude, t1, d):
@@ -75,7 +77,6 @@ class UniformNoise:
 
     @staticmethod
     def plot_uniform_noise(amplitude, t1, d, sampling_rate):
-
         uniform_noise = amplitude * (2 * np.random.rand(int(d * sampling_rate)) - 1)
         t = np.linspace(t1, t1 + d, len(uniform_noise))
 
@@ -100,7 +101,6 @@ class GaussianNoise:
 
     @staticmethod
     def plot_gaussian_noise(amplitude, mean, dev, t1, d, sampling_rate):
-
         noise = amplitude * np.random.normal(mean, dev, sampling_rate)
         t = np.linspace(t1, t1 + d, len(noise))
 
@@ -118,4 +118,84 @@ class GaussianNoise:
 
         plt.hist(noise, bins_arr, edgecolor='black')
         plt.grid()
+        plt.show()
+
+
+"""
+Sygnały susła poniżej
+"""
+
+
+class RectangularSignal:
+    def __init__(self, range_start, range_length, amplitude, term, fulfillment):
+        self.range_start = range_start
+        self.range_length = range_length
+        self.amplitude = amplitude
+        self.term = term
+        self.fulfillment = fulfillment
+
+    def value(self, t):
+        if ((t - self.range_start) / self.term) - np.floor((t - self.range_start) / self.term) < self.fulfillment:
+            return self.amplitude
+        else:
+            return 0.0
+
+    def generate_signal(self):
+        t = np.linspace(self.range_start, self.range_start + self.range_length, int(1000 * self.range_length))
+        signal = np.array([self.value(ti) for ti in t])
+        return t, signal
+
+    def plot_signal(self):
+        t, signal = self.generate_signal()
+        plt.plot(t, signal)
+        plt.title('Rectangular Signal')
+        plt.xlabel('Time')
+        plt.ylabel('Amplitude')
+        plt.grid(True)
+        plt.show()
+
+    def plot_histogram(self):
+        _, signal = self.generate_signal()
+        plt.hist(signal, bins='auto', edgecolor='black')
+        plt.title('Histogram of Rectangular Signal')
+        plt.xlabel('Amplitude')
+        plt.ylabel('Frequency')
+        plt.grid(True)
+        plt.show()
+
+class RectangularSymmetricSignal:
+    def __init__(self, range_start, range_length, amplitude, term, fulfillment):
+        self.range_start = range_start
+        self.range_length = range_length
+        self.amplitude = amplitude
+        self.term = term
+        self.fulfillment = fulfillment
+
+    def value(self, t):
+        if ((t - self.range_start) / self.term) - np.floor((t - self.range_start) / self.term) < self.fulfillment:
+            return self.amplitude
+        else:
+            return -self.amplitude
+
+    def generate_signal(self):
+        t = np.linspace(self.range_start, self.range_start + self.range_length, int(1000 * self.range_length))
+        signal = np.array([self.value(ti) for ti in t])
+        return t, signal
+
+    def plot_signal(self):
+        t, signal = self.generate_signal()
+        plt.plot(t, signal)
+        plt.title('Rectangular Symmetric Signal')
+        plt.xlabel('Time')
+        plt.ylabel('Amplitude')
+        plt.grid(True)
+        plt.show()
+
+    def plot_histogram(self):
+        _, signal = self.generate_signal()
+        plt.hist(signal, bins='auto', edgecolor='black')
+        plt.title('Histogram of Rectangular Symmetric Signal')
+        plt.xlabel('Amplitude')
+        plt.ylabel('Frequency')
+        plt.grid(True)
         plt.show()
--- DSP/zad1/main.py
+++ DSP/zad1/main.py
@@ -4,6 +4,23 @@ from SignalGenerator import *
 # UniformNoise.plot_uniform_noise(1, 0, 1, 1000)
 # GaussianNoise.plot_gaussian_noise(2, 0, 1, 0, 2, 1000)
 # SinusSignal.plot_sinus_signal_full(1, 1000, 0, 2, 2)
-SinusSignal.create_sinus_histogram(2,1000,0,5,0,2)
-UniformNoise.histogram(2,0,2,1000,10)
-GaussianNoise.histogram(2,0,1,1000,20)
+# SinusSignal.create_sinus_histogram(2, 1000, 0, 5, 0, 2)
+# UniformNoise.histogram(2, 0, 2, 1000, 10)
+# GaussianNoise.histogram(2, 0, 1, 1000, 20)
+
+
+t1 = 0  # Początek zakresu
+d = 4  # Długość zakresu
+a = -1  # Amplituda
+t = 1  # Okres
+k = 0.25  # Wypełnienie
+
+# PROSTOKATNY
+signal = RectangularSignal(t1, d, a, t, k)
+signal.plot_signal()
+signal.plot_histogram()
+
+# PROSTOKATNY SYMETRYCZNY
+symmetric_signal = RectangularSymmetricSignal(t1, d, a, t, k)
+symmetric_signal.plot_signal()
+symmetric_signal.plot_histogram()
