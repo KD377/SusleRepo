4a54e106c081b84a29c6f379a2b06fd3e7c2d85d
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2024-03-14 21:28:23 +0100

    DE algorithm for optimizing 20 dimensional functions

--- TIAD/DF/main.py
+++ TIAD/DF/main.py
@@ -1,40 +1,62 @@
 import numpy as np
 
 
-def differential_evolution(fitness_func, bounds, pop_size=50, max_iter=1000, F=0.5, CR=0.9, tol=1e-5, dimension=20):
+def sphere_function(x):
+    return np.sum(x ** 2)
 
-    dim = len(bounds)
-    lower_bound, upper_bound = np.array(bounds).T
-    pop = np.random.rand(pop_size, dim) * (upper_bound - lower_bound) + lower_bound
-    fitness = np.array([fitness_func(ind) for ind in pop])
-    best_idx = np.argmin(fitness)
-    best_solution, best_fitness = pop[best_idx], fitness[best_idx]
 
-    for i in range(max_iter):
-        for j in range(pop_size):
-            idxs = [idx for idx in range(pop_size) if idx != j]
-            a, b, c = pop[np.random.choice(idxs, 3, replace=False)]
-            mutant = pop[j] + F * (best_solution - pop[j]) + F * (a - b)
-            mutant = np.clip(mutant, lower_bound, upper_bound)
-            crossover = np.random.rand(dim) < CR
-            if not np.any(crossover):
-                crossover[np.random.randint(0, dim)] = True
-            trial = np.where(crossover, mutant, pop[j])
-            trial_fitness = fitness_func(trial)
-            if trial_fitness < fitness[j]:
-                pop[j], fitness[j] = trial, trial_fitness
-                if trial_fitness < best_fitness:
-                    best_solution, best_fitness = trial, trial_fitness
-        if np.std(fitness) < tol:
+def crossover(parent_individual, mutant_individual, CR):
+
+    D = len(parent_individual)
+
+    d = np.random.randint(0, D)
+
+    crossover_individual = np.array(
+        [mutant_individual[j] if (np.random.rand() < CR or j == d) else parent_individual[j] for j in range(D)])
+
+    return crossover_individual
+
+
+def differential_evolution(function, bounds, dimensions, population_size, max_iterations, F=0.5, CR = 0.9, tolerance=1e-4):
+    population = np.random.uniform(bounds[0], bounds[1], size=(population_size, dimensions))
+
+    for i in range(max_iterations):
+        new_population = np.zeros_like(population)
+
+        for j in range(population_size):
+            candidates = np.random.choice(population_size,2,replace=False)
+            a, b = population[candidates]
+
+            lambda_value = np.random.rand()
+
+            mutant = lambda_value * population[np.argmin([function(vector) for vector in population])] + (1 - lambda_value) * population[j] + F * (a - b)
+
+            trial_vector = crossover(population[j], mutant, CR)
+            if function(trial_vector) < function(population[j]):
+                new_population[j] = trial_vector
+            else:
+                new_population[j] = population[j]
+
+        population = new_population
+
+        best_fitness = function(population[np.argmin([function(vector) for vector in population])])
+        if best_fitness < tolerance:
             break
 
-    return best_solution, best_fitness
+    best_individual = population[np.argmin([function(vector) for vector in population])]
 
-# Example usage:
-def sphere_function(x):
-    return np.sum(x**2)
+    return best_individual
 
-bounds = [(-100, 100)] * 20
-best_solution, best_fitness = differential_evolution(sphere_function, bounds)
-print("Best solution:", best_solution)
-print("Best fitness:", best_fitness)
+
+bounds = (-100, 100)
+dimensions = 20
+population_size = 50
+max_iterations = 1000
+F = 0.5
+CR = 0.9
+tolerance = 1e-4
+
+best_solution = differential_evolution(sphere_function, bounds, dimensions, population_size, max_iterations, F, CR, tolerance)
+print("Best solution found:", best_solution)
+print("Fitness value of the best solution:", sphere_function(best_solution))
+
