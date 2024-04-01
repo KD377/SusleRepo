680b56f22e3b40153e7e6bbc4f24e63c03dd287f
242377 Deka Krzysztof <242377@edu.p.lodz.pl>
2024-04-01 14:47:35 +0200

    Added logic for feature extraction left place for implementation in Feature extractor. NOTE: Look up Article class method createFetureVector

--- KSR/zad1/src/main/java/org/example/Article.java
+++ KSR/zad1/src/main/java/org/example/Article.java
@@ -6,11 +6,19 @@ public class Article {
     private String body;
     private String places;
 
+    private Object[] featureVector;
+
     public Article(String date, String title, String body, String places) {
         this.date = date;
         this.title = title;
         this.body = body;
         this.places = places;
+        this.featureVector = new Object[10];
+    }
+
+    public Object[] createFeatureVector() throws Exception {
+        throw new Exception("Not implemented"); // USE FOR THIS FEATURE EXTRACTOR Utility class methods
+        //Implement them first then use article body as argument
     }
 
     @Override
